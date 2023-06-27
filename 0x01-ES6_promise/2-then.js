514c240b65cc77c509932df6866c840732835b51
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-06-27 22:43:00 +0300

    introduction to promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,17 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
 
-    .catch(() => {
-      return Error();
-    })
-
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
