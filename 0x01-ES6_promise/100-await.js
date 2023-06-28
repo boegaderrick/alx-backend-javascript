fcf04a15f65925abdf8a971a440a97aef273cb2c
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-06-28 17:11:27 +0300

    await promise in async function

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploaderUser() {
-  return Promise.all([await uploadPhoto(), await createUser()])
-    .then(([photo, user]) => ({ photo, user }))
-    .catch(() => ({ photo: null, user: null }));
+  // return Promise.all([await uploadPhoto(), await createUser()])
+  //  .then(([photo, user]) => ({ photo, user }))
+  //  .catch(() => ({ photo: null, user: null }));
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
 }
