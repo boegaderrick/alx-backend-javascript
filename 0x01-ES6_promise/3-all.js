d5fe82d830fe8b9de5e628dcabfaed870bb7a897
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-06-28 10:27:10 +0300

    handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,11 +2,12 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const { body } = values[0];
-      const fname = values[1].firstName;
-      const lname = values[1].lastName;
-      console.log(`${body} ${fname} ${lname}`);
+    .then(([photo, user]) => {
+      // const { body } = values[0];
+      // const fname = values[1].firstName;
+      // const lname = values[1].lastName;
+      // console.log(`${body} ${fname} ${lname}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
