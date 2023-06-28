ae1031830e202af0a4d31cbc4722318c2988870d
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-06-28 12:42:19 +0300

    settle all promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     // .then((values) => console.log(values))
     // .catch((error) => console.log(error));
     .then((values) => values)
