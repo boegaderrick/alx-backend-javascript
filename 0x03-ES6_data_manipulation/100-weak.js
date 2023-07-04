b5ff64cb731b5a67e065f5968e9313da461fcddb
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-04 15:53:25 +0300

    Weak Link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,13 +2,17 @@ const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    if (weakMap.get(endpoint) >= 5) {
+    /*if (weakMap.get(endpoint) >= 5) {
       throw new Error('Endpoint load is high');
-    }
+    }*/
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   } else {
     weakMap.set(endpoint, 1);
   }
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
 }
 
 export { weakMap, queryAPI };
