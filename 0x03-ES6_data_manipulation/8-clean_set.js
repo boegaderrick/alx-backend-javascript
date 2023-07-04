a133fe94cb79b7ed3054ab39c588dfcbc6fea799
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-07-04 15:34:03 +0300

    slice and join strings

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === undefined || typeof startString !== 'string' || startString.length < 1) {
+  if (typeof startString !== 'string' || startString.length < 1) {
     return '';
   }
   return [...set].filter((element) => element.startsWith(startString)).join('-').split(startString).join('');
