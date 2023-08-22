9122d06b6e4c558895b88059699bbecca764897e
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-22 18:52:10 +0300

    node js basics

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const countStudents = function (fileName) {
+  const fs = require('fs');
+  let outStr = '';
+  return new Promise((resolve, reject) => {
+    if (!fileName) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    fs.readFile(fileName, 'utf8', function (error, fileData) {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const data = fileData.split('\n').filter(line => line.trim() !== '');
+        outStr = `${outStr}Number of students: ${data.length - 1}\n`;
+        const fields = new Set();
+        data.slice(1).forEach((row) => {
+          fields.add(row.split(',')[3]);
+        });
+        fields.forEach((field) => {
+          const students = data.slice(1).filter(row => row.includes(field));
+          outStr = `${outStr}Number of students in ${field}: ${students.length}. List: `;
+          for (let i = 0; i < students.length; i++) {
+            outStr = `${outStr}${students[i].split(',')[0]}`;
+            if (i < students.length - 1) {
+              outStr = `${outStr}, `;
+            } else {
+              outStr = `${outStr}\n`;
+            }
+          }
+        });
+        resolve(outStr);
+      }
+    });
+  });
+};
+
+app.get('/students', (request, response) => {
+  countStudents(process.argv[2])
+    .then((outStr) => {
+      response.send(outStr.slice(1, -1));
+    })
+    .catch((error) => {
+      console.log(error);
+      response.send('Cannot load the database');
+    });
+});
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
