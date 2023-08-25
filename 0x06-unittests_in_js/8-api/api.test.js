cea6128f7109a9147223b3fcc1fc143e04b52a65
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-25 16:40:10 +0300

    unittests in js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,7 +2,7 @@ const request = require('request');
 const url = 'http://localhost:7865';
 const expect = require('chai').expect;
 
-describe('', function () {
+describe('GET /', function () {
   it('Test GET /', function () {
     request(url, (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
