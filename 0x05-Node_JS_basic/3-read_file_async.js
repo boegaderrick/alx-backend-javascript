288071016f1014b9e394015a681d408e66406abb
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-25 22:54:39 +0300

    console.log bugfix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,22 +6,21 @@ module.exports = function countStudents (fileName) {
         reject(new Error('Cannot load the database'));
       } else {
         const data = fileData.split('\n').filter(line => line.trim() !== '');
-        process.stdout.write(`Number of students: ${data.length - 1}\n`);
+        console.log(`Number of students: ${data.length - 1}`);
         const fields = new Set();
         data.slice(1).forEach((row) => {
           fields.add(row.split(',')[3]);
         });
         fields.forEach((field) => {
           const students = data.slice(1).filter(row => row.includes(field));
-          process.stdout.write(`Number of students in ${field}: ${students.length}. List: `);
+          let outStr = `Number of students in ${field}: ${students.length}. List: `;
           for (let i = 0; i < students.length; i++) {
-            process.stdout.write(`${students[i].split(',')[0]}`);
+            outStr += `${students[i].split(',')[0]}`;
             if (i < students.length - 1) {
-              process.stdout.write(', ');
-            } else {
-              console.log();
+              outStr += ', ';
             }
           }
+          console.log(outStr);
         });
         resolve();
       }
