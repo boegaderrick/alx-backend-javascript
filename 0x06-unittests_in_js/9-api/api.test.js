b1caf40d30a2099121bb3d0eb4ab99c91bfde4ee
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-25 04:18:11 +0300

    unittests in js

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -12,15 +12,15 @@ describe('Index page', function () {
   });
 });
 
-describe('Index', function () {
-  it('Tests when id is a number', function () {
+describe('Carts page', function () {
+  it('', function () {
     request('http://localhost:7865/cart/12', (error, response, body) => {
       expect(body).to.equal('Payment methods for cart 12\n');
       expect(response.statusCode).to.equal(200);
     });
   });
 
-  it('Tests when id is not a number', function () {
+  it('', function () {
     request('http://localhost:7865/cart/test', (error, response, body) => {
       expect(response.statusCode).to.equal(404);
     });
